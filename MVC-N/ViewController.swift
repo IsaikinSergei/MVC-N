a4d35279067f4ade8ef39e543db8d77f95b474e9
IsaikinSergei <akinfest007@gmail.com>
2021-02-24 19:10:29 +0400

    Initial Commit

---
+++ MVC-N/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC-N
+//
+//  Created by Sergei Isaikin on 24.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
